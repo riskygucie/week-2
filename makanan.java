9eac67213701cc0fb597a3cfd69adfe5d7483c9c
riskygucie <riskygucie@gmail.com>
2026-05-05 10:20:06 +0700

    membuat inheritance makanan dan elektronik

---
+++ makanan.java
@@ -0,0 +1,7 @@
+public class makanan extends Produk {
+    private int expired;
+    public makanan(String nama, double harga, int stok, String kategori){
+        super(nama, harga, stok, kategori);
+        
+    }
+}
