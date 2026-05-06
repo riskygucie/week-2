9b1de769dd29350a610348ff34dec0b2fcaa5e62
riskygucie <riskygucie@gmail.com>
2026-05-06 09:55:47 +0700

    nambahin function pada elektronik

--- elektronik.java
+++ elektronik.java
@@ -3,6 +3,9 @@ public class elektronik extends Produk {
     private int garansiTahun;
     private static final double PPN = 0.11;
 
+    public elektronik(String nama, String merek, double harga, int stok, int garansiTahun) {
+        super(nama, harga, stok);
+        this.merek = merek;
+        this.garansiTahun = garansiTahun;
     }
-
-
+}
