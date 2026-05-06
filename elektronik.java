2de72a78c524e8ed9f5df8f6b4991fe5be98c913
riskygucie <riskygucie@gmail.com>
2026-05-06 10:08:13 +0700

    benerin kurung

--- elektronik.java
+++ elektronik.java
@@ -1,8 +1,7 @@
 public class elektronik extends Produk {
     private String merek;
     private int garansiTahun;
-    private static final double PPN = 0.11;
-}
+
 
     public elektronik(String nama, String merek, double harga, int stok, int garansiTahun) {
         super(nama, harga, stok);
@@ -42,6 +41,6 @@ public class elektronik extends Produk {
     public double hitungPajak(double harga) {
         return harga * 0.20;
     }
-
+}
 
 
