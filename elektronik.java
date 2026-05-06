3038f9a7165415aff03992b4df7878148f8c450d
riskygucie <riskygucie@gmail.com>
2026-05-06 10:06:59 +0700

    nambahin kurung dan override kemudian membuat function hitung pajak

--- elektronik.java
+++ elektronik.java
@@ -2,6 +2,7 @@ public class elektronik extends Produk {
     private String merek;
     private int garansiTahun;
     private static final double PPN = 0.11;
+}
 
     public elektronik(String nama, String merek, double harga, int stok, int garansiTahun) {
         super(nama, harga, stok);
@@ -25,7 +26,7 @@ public class elektronik extends Produk {
         System.out.println("Merek    : " + merek);
         System.out.println("Garansi  : " + garansiTahun + " tahun");
         System.out.println("----------");
-
+    }
         public double hitungDiskon(int jumlahBeli) {
         return hitungTotalHarga(jumlahBeli) * PPN;
     }
@@ -33,6 +34,14 @@ public class elektronik extends Produk {
     public double hitungDiskon(int jumlahBeli, double persenPajak) {
         return hitungTotalHarga(jumlahBeli) * (persenPajak / 100);
     }
+    public double hitungHargaSetelahPajak(int jumlahBeli) {
+        return hitungTotalHarga(jumlahBeli) + hitungPajak(jumlahBeli);
     }
 
-}
+    @Override
+    public double hitungPajak(double harga) {
+        return harga * 0.20;
+    }
+
+
+
