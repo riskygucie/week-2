b8a915333fd2e692d400676626f48fa986993e9b
riskygucie <riskygucie@gmail.com>
2026-05-06 10:28:23 +0700

    nambahin method jumlah beli

--- Produk.java
+++ Produk.java
@@ -29,8 +29,25 @@ abstract class Produk {
     public void setStok(int stok) {
         this.stok = stok;
     }
-      public double hitungTotalHarga(int jumlahBeli) {
+
+    public double hitungTotalHarga(int jumlahBeli) {
         return harga * jumlahBeli;
     }
 
+    public void beli(int jumlahBeli) {
+        if (jumlahBeli <= 0) {
+            System.out.println("Jumlah beli harus lebih dari 0!");
+            return;
+        }
+
+        if (jumlahBeli > stok) {
+            System.out.println("Stok " + nama + " tidak cukup! Tersisa: " + stok);
+            return;
+        }
+
+        stok -= jumlahBeli;
+        System.out.println("Beli " + nama + " x" + jumlahBeli +
+                " = Rp" + hitungTotalHarga(jumlahBeli) +
+                " | Sisa stok: " + stok);
+    }
 }
