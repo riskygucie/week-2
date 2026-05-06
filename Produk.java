35fcd0687ce0a64f0ac2f4c966a568128a3c7d70
riskygucie <riskygucie@gmail.com>
2026-05-06 10:22:44 +0700

    nambahin kontraktor pada produk

--- Produk.java
+++ Produk.java
@@ -1,8 +1,13 @@
 
-
 abstract class Produk {
-       private String nama;
+    private String nama;
     private double harga;
     private int stok;
 
+    public Produk(String nama, double harga, int stok) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
     }
+
+}
