4fd64dc220a88225ede15890f0d6a7cd2456698d
riskygucie <riskygucie@gmail.com>
2026-05-06 10:10:55 +0700

    nambahin objek pada kelas makanan

--- makanan.java
+++ makanan.java
@@ -1,21 +1,12 @@
 public class makanan extends Produk {
-    private int expired;
-    public makanan(String nama, double harga, int stok, String kategori, int expired){
-        super(nama, harga, stok, kategori);
-        this.expired = expired;        
-    } 
-    @Override
-    public void tampilinfo(){
-        super.tampilinfo();
-        System.out.println("masa expired: " + expired + "hari");
-    }
-    public void setexpired(int expired){
-        if (expired < 0){
-            System.out.println("masa expired tidak boleh negative");
+     private String kedaluwarsa;
+    private String kategori;
 
-        }else{
-            this.expired = expired;
-        }
+    public makanan(String nama, double harga, int stok, String kedaluwarsa, String kategori) {
+        super(nama, harga, stok);
+        this.kedaluwarsa = kedaluwarsa;
+        this.kategori = kategori;
     }
+
 
 }
