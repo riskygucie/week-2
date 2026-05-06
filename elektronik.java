f893b00808941c71aa9d83d8c95b001db421e8a3
riskygucie <riskygucie@gmail.com>
2026-05-06 09:53:56 +0700

    nambahin objek di elektronik

--- elektronik.java
+++ elektronik.java
@@ -1,20 +1,8 @@
 public class elektronik extends Produk {
-    private int garansi;
-    public elektronik(String nama, double harga, int stok, String kategori, int garansi){
-        super(nama, harga, stok, kategori);
-        this.garansi = garansi;        
-    } 
-    @Override
-    public void tampilinfo(){
-        super.tampilinfo();
-        System.out.println("masa garansi: " + garansi + "hari");
-    }
-    public void setgaransi(int garansi){
-        if (garansi <= 0){
-            System.out.println("masa garansi tidak boleh negative");
-        }else{
-            this.garansi = garansi;
-        }
+    private String merek;
+    private int garansiTahun;
+    private static final double PPN = 0.11;
+
     }
 
-}
+
