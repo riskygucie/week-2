cc076f115c3b86d7eea2efeba46613ace4b2f1c0
riskygucie <riskygucie@gmail.com>
2026-05-13 22:08:26 +0700

    nambahin override dan konstruktor tampilin info

--- makanan.java
+++ makanan.java
@@ -9,5 +9,12 @@
     public double hitungHargaAkhir(int jmlhBeli) {
         System.out.println("Total Harga= " + hitungTotalHarga(jmlhBeli));
         return hitungTotalHarga(jmlhBeli) * 0.95;
+
+    }
+
+@Override
+    public void tampilInfo() {
+        super.tampilInfo();
+        System.out.println("Masa Kaduluarsa: " + kadaluarsa + " Hari");
     }
 }
