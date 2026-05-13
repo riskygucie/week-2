cb4eb96dbdf29bdde7b40246368649efa5b2def4
riskygucie <riskygucie@gmail.com>
2026-05-13 22:16:21 +0700

    nambahin polytmorph

--- kasir.java
+++ kasir.java
@@ -7,4 +7,18 @@ public class kasir {
         System.out.println("===== TOKO GARUDA =====");
         System.out.println("Nama Kasir: " + namaKasir);
     }
+    public void prosesBelanja(Produk p, int jmlhBeli) {
+
+        if (p.getStok() < jmlhBeli) {
+            System.out.println("Out of stok");
+        }
+        {
+            double hargaAkhir = p.hitungHargaAkhir(jmlhBeli);
+            System.out.println("Harga Akhir = " + hargaAkhir);
+            p.setStok(jmlhBeli);
+        }
+
+        System.out.println("Stok terbaru= " + p.getStok());
+
+    }
 }
