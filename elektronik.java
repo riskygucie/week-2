a9a5804b00314e9ff63bef8688e071e3a6dc85a9
riskygucie <riskygucie@gmail.com>
2026-05-13 22:01:46 +0700

    nambahin konstruktor dan inheritence

--- elektronik.java
+++ elektronik.java
@@ -1,46 +1,11 @@
 class elektronik extends Produk {
-    private String merek;
-    private int garansiTahun;
-       private static final double PPN = 0.11;
+  private int masaGaransi;
 
-    public elektronik(String nama, String merek, double harga, int stok, int garansiTahun) {
+    public Elektronik(String nama, double harga, int stok, int masaGaransi) {
         super(nama, harga, stok);
-        this.merek = merek;
-        this.garansiTahun = garansiTahun;
-
+        this.masaGaransi = masaGaransi;
+    }
     }
 
-    public String getMerek() {
-        return merek;
-    }
-
-    public int getGaransiTahun() {
-        return garansiTahun;
-    }
-     @Override
-    public void tampilinfo() {
-        System.out.println("[ELEKTRONIK]");
-        System.out.println("Nama     : " + getnama());
-        // super.tampilInfo(); 
-        System.out.println("Merek    : " + merek);
-        System.out.println("Garansi  : " + garansiTahun + " tahun");
-        System.out.println("----------");
-    }
-        public double hitungDiskon(int jumlahBeli) {
-        return hitungTotalHarga(jumlahBeli) * PPN;
-    }
-
-    public double hitungDiskon(int jumlahBeli, double persenPajak) {
-        return hitungTotalHarga(jumlahBeli) * (persenPajak / 100);
-    }
-    public double hitungHargaSetelahPajak(int jumlahBeli) {
-        return hitungTotalHarga(jumlahBeli) + hitungPajak(jumlahBeli);
-    }
-
-    @Override
-    public double hitungPajak(double harga) {
-        return harga * 0.20;
-    }
-}
 
 
