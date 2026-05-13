b4b2b64ac46d015724de15efd025a3cf4ba81ebd
riskygucie <riskygucie@gmail.com>
2026-05-13 21:54:24 +0700

    nambahin constructor

--- Produk.java
+++ Produk.java
@@ -3,6 +3,18 @@ abstract class Produk {
     private String nama;
     private double harga;
     private int stok;
-    
-    
+
+    public Produk(String nama, double harga, int stok) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
+
+    public void setStok(int jmlBeli) {
+        stok = stok - jmlBeli;
+    }
+
+    public int getStok() {
+        return stok;
+    }
 }
