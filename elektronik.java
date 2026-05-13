02ad02bdf3ec22a3e3945eb8941ba481cbd9ee8f
riskygucie <riskygucie@gmail.com>
2026-05-13 22:02:49 +0700

    nambahinb konstruktor hitung harga

--- elektronik.java
+++ elektronik.java
@@ -5,6 +5,13 @@ class elektronik extends Produk {
         super(nama, harga, stok);
         this.masaGaransi = masaGaransi;
     }
+        @Override
+    public double hitungHargaAkhir(int jmlhBeli) {
+        System.out.println("Total Harga= " + hitungTotalHarga(jmlhBeli));
+        return hitungTotalHarga(jmlhBeli) * 0.85;
+    
+
+    }
     }
 
 
